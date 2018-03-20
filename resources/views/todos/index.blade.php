38f82342d8349342304879446d9922654a2d4fca
dudedean <aima10.aima11@gmail.com>
2018-03-20 14:37:11 +0800

    TodoList App with working index pages,migration and layouts.

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Todos</h1>
+
+@if(count($todos)>0)
+
+    @foreach($todos as $todo)
+
+        <div class="card mt-3">
+            <h3>{{$todo->text}} <span class="badge badge-danger">{{$todo->due}}</span></h3> 
+            
+        </div>
+
+    @endforeach
+
+@endif
+
+@endsection
